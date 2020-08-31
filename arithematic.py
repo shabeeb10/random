a5bf84f710a1253560d8f108324ea877d1fa6ef3
shabeeb10 <63544231+shabeeb10@users.noreply.github.com>
2020-08-31 20:15:12 +0530

    Add files via upload

---
+++ arithematic.py
@@ -0,0 +1,12 @@
+a= int(input('Enter First number: '))
+b = int(input('Enter Second number '))
+add = a + b
+dif =  a- b
+mul =  a* b
+div =  a/ b
+rem = a //b
+print('Sum of ',a ,'and' ,b ,'is :',add)
+print('Difference of ',a ,'and' ,b ,'is :',dif)
+print('Product of' ,a ,'and' ,b ,'is :',mul)
+print('Division of ',a ,'and' ,b ,'is :',div)
+print('Modulus of ',a ,'and' ,b ,'is :',rem)
